81a1f5b3ce8a2138208519fa4b305980a0e51763
aicha652 <aichaazlf652@gmail.com>
2024-06-10 13:54:48 +0200

    Create Functions that Clear Inputs & Read Data

--- main.js
+++ main.js
@@ -43,5 +43,48 @@ submit.onclick = () => {
         category: category.value
     }
     dataPro.push(newPro)
+    //save localstorage
     localStorage.setItem('product', JSON.stringify(dataPro))
+
+    clearData()
+    readData()
 }
+
+
+//clear inputs
+
+function clearData() {
+    title.value = ' '
+    price.value = ' '
+    taxes.value = ' '
+    ads.value = ' '
+    discount.value = ' '
+    total.innerHTML = ' '
+    count.value = ' '
+    category.value = ' '
+}
+
+
+//read Data
+
+let readData = () => {
+    let table = ' '
+    for(let i = 0; i < dataPro.length; i++) {
+        table = table + `
+                    <tr>
+                        <td>${dataPro[i].title}</td>
+                        <td>${dataPro[i].price}</td>
+                        <td>${dataPro[i].taxes}</td>
+                        <td>${dataPro[i].ads}</td>
+                        <td>${dataPro[i].discount}</td>
+                        <td>${dataPro[i].total}</td>
+                        <td>${dataPro[i].count}</td>
+                        <td>${dataPro[i].category}</td>
+                        <td><button id="update">update</button></td>
+                        <td><button id="delete">delete</button></td>
+                    </tr>
+        ` 
+    }
+    document.getElementById('tbody').innerHTML = table
+}
+readData()
